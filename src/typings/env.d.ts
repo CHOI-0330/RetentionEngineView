fe7694d8795e848ff27d4f9212a3039efb29a07a
CHOI  SEONGUN <apple000330@naver.com>
2025-12-10 01:36:26 +0900

    testコード削除

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -3,9 +3,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_SUPABASE_URL?: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY?: string;
     SUPABASE_SERVICE_ROLE_KEY?: string;
-    NEXT_PUBLIC_ENABLE_GEMINI?: string;
-    GEMINI_API_KEY?: string;
-    GEMINI_MODEL_ID?: string;
     NEXT_PUBLIC_API_BASE_URL?: string;
   }
 }
